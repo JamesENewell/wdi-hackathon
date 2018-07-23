f8409100995d025b2f4f0209d72bedfcd374eb16
Bianca Jemsten <biancajemsten@gmail.com>
2018-07-23 12:43:18 +0100

    started to build out google map

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="map"></div>
+</template>
+
+<script>
+/* global google*/
+export default {
+  name: 'GoogleMap',
+  props: ['center', 'venues'],
+  mounted() {
+    this.map = new google.maps.Map(this.$el, {
+      center: this.center || { lat: 51.515, lng: -0.078 },
+      zoom: 14
+    });
+
+    this.infoWindow = new google.maps.InfoWindow();
+  },
+  watch: {
+  places() {
+    this.bounds = new google.maps.LatLngBounds();
+
+    this.markers = this.places.map(place => {
+      this.bounds.extend(place.location);
+
+      const marker = new google.maps.Marker({
+        position: place.location,
+        map: this.map
+      });
+
+    });
+
+    this.map.panTo(this.bounds.getCenter());
+  },
+  center() {
+    this.map.setCenter(this.center);
+    this.marker = new google.maps.Marker({
+      position: this.center,
+      map: this.map
+    });
+  }
+}
+}
+</script>
