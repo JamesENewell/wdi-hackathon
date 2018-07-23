caac4e6cd97ebf3e5e3e2ad5cb181a407b9edac3
MartinKoeoep <you@example.com>
2018-07-23 13:50:40 +0100

    played around with getting form to populate

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,24 +2,28 @@
   <section class="section">
     <div class="container">
       <h1>Newgle</h1>
-      <form>
+      <form v-on:submit.prevent="handleSubmit()">
         <label for="search">What are you looking for?</label>
-        <input class="input" type="text" v-model="textInput"></input>
-        <select v-model="type">
+        <input class="input" type="text" v-model="search.textInput"></input>
+        <p>Message goes here {{ textInput}}</p>
+        <select v-model="search.type">
           <option value="bar">Bar</option>
           <option value="gym">Gym</option>
         </select>
+        <button type="button" name="button">El Submit</button>
       </form>
-      <google-map v-bind:places="places" />
+      <GoogleMap v-bind:places="places" />
 
     </div>
   </section>
 </template>
 
 <script>
-
 import axios from 'axios';
 import GoogleMap from './GoogleMap';
+import Vue from 'vue';
+
+
 
 function findGooglePlaces(req, res){
   console.log('req', req)
@@ -38,10 +42,34 @@ function findGooglePlaces(req, res){
 
 export default {
   name: 'Home',
-  components: 'GoogleMap'
-
+  data() {
+    return {
+      search: {
+      }
+    }
+  },
+  components: {
+      GoogleMap
+  },
+  watch:{
+    search(){
+      console.log('this');
+    }
+  },
+  methods: {
+    handleSubmit() {
+      console.log(data);
+    },
+    handlePlaceChange({ formatted_address: address, geometry: { location } }) {
+      this.venue.address = address;
+      this.venue.location = location.toJSON();
+    }
+  },
 };
 </script>
 
 <style>
+.map {
+  height: calc(100vh - 52px);
+}
 </style>
