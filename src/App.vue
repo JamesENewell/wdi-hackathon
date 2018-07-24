4078147b8447df225f1fea6d1dab8a2edac3d680
Bianca Jemsten <biancajemsten@gmail.com>
2018-07-24 15:04:46 +0100

    spinning globe

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,11 @@
         </a>
       </div>
       <div class="navbar-menu" v-bind:class="{ 'is-active': navIsOpen }">
+        <div class="navbar-start">
+          <div class="globeContainer">
+          <img class="globe" src="http://www.pngall.com/wp-content/uploads/2016/06/Globe.png"/>
+        </div>
+        </div>
         <div class="navbar-end">
           <router-link to="/" class="navbar-item">Home</router-link>
           <router-link to="/about" class="navbar-item">About</router-link>
@@ -51,6 +56,27 @@ nav.navbar{
 a.navbar-item:hover{
   background-color:  rgba(255,182,193, 0.2) !important;
   color: black !important;
+}
+
+img.globe{
+  height: 50px;
+  width: auto;
+}
+
+.globeContainer{
+  animation: spin 2000ms linear infinite, moveLeftToRight 5s linear 1;
+  position: absolute;
+}
+
+@keyframes spin {
+  from { transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
+}
+
+
+@keyframes moveLeftToRight {
+  0%   { left: -100px; }
+  100% { left: 100%; }
 }
 
 
