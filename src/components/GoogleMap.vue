70c4b1c18f749ee2226d1ebf7494640667b7825d
Bianca Jemsten <biancajemsten@gmail.com>
2018-07-24 10:51:01 +0100

    styled map

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -10,7 +10,87 @@ export default {
   mounted() {
     this.map = new google.maps.Map(this.$el, {
       center: this.center || { lat: 51.515, lng: -0.078 },
-      zoom: 2
+      zoom: 2,
+      styles: [
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#444444"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#aad2e3"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    }
+]
     });
 
     this.infoWindow = new google.maps.InfoWindow();
@@ -23,9 +103,14 @@ export default {
     this.bounds = new google.maps.LatLngBounds();
     this.markers = this.places.map(place => {
       this.bounds.extend(place.location);
+      const icon = {
+        url: place.flag, // url
+        scaledSize: new google.maps.Size(20, 12), // scaled size
+      };
       const marker = new google.maps.Marker({
         position: place.location,
-        map: this.map
+        icon: icon,
+        map: this.map,
       });
       marker.addListener('click', () => {
         this.infoWindow.setContent(`
